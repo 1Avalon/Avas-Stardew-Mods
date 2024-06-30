b73c9d9a79a1c474bc1e15ae3855d2085b265802
Avalon <axj30.11.04@gmail.com>
2024-06-30 15:03:25 +0100

    network code

--- FriendshipStreaks/ModEntry.cs
+++ FriendshipStreaks/ModEntry.cs
@@ -9,6 +9,7 @@ using StardewValley.Menus;
 using Microsoft.Xna.Framework.Graphics;
 using System.Linq.Expressions;
 using System.IO;
+using System.Threading;
 
 namespace FriendshipStreaks
 {
@@ -20,12 +21,18 @@ namespace FriendshipStreaks
         public static Texture2D gameCursors;
 
         public static Dictionary<string, FriendshipStreak> streaks = new Dictionary<string, FriendshipStreak>();
+
+        public static NetworkDataManager networkDataManager;
+
+        public bool skipResettingStreaks = true; //Only for farmhands
         public override void Entry(IModHelper helper)
         {
 
             Helper.Events.GameLoop.Saving += OnSaving;
             Helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
             Helper.Events.GameLoop.DayStarted += OnDayStarted;
+            Helper.Events.Multiplayer.ModMessageReceived += OnModMessageReceived;
+            Helper.Events.Multiplayer.PeerConnected += OnPeerConnected;
 
             Helper.ConsoleCommands.Add("set_streak", "Sets the streak for an NPC to the given value.\nUsage: set_streak <type> <NPC name> <value>\n- type - must be either 'gift' or 'talking'\n- NPC name - the name of your target\n- value - amount of the desired streak.", this.SetStreak);
 
@@ -61,20 +68,75 @@ namespace FriendshipStreaks
         }
         private void OnDayStarted(object sender, DayStartedEventArgs e)
         {
+            if (skipResettingStreaks && !Context.IsMainPlayer)
+            {
+                skipResettingStreaks = false;
+                return;
+            }
+
             foreach (KeyValuePair<string, FriendshipStreak> kvp in streaks)
             {
                 kvp.Value.ResetStreaksIfMissed();
             }
+            if (!Context.IsMainPlayer)
+            {
+                networkDataManager.SendDataToHost();
+            }
         }
         private void OnSaving(object sender, SavingEventArgs e)
         {
-            foreach (KeyValuePair<string, FriendshipStreak> kvp in streaks)
+            if (!Context.IsMainPlayer)
             {
-                Helper.Data.WriteSaveData(kvp.Key, kvp.Value);
-                Monitor.Log($"Saved Streak data for {kvp.Key}");
+                networkDataManager.SendDataToHost();
+                Monitor.Log("Sent data to host");
+            }
+            else
+            {
+                Helper.Data.WriteSaveData<NetworkDataManager>("NetworkDataManager", networkDataManager);
+                foreach (KeyValuePair<string, FriendshipStreak> kvp in streaks)
+                {
+                    Helper.Data.WriteSaveData(kvp.Key, kvp.Value);
+                    Monitor.Log($"Saved Streak data for {kvp.Key}");
+                }
             }
         }
 
+        private void OnModMessageReceived(object sender, ModMessageReceivedEventArgs e)
+        {
+            if (networkDataManager == null)
+                networkDataManager = new NetworkDataManager();
+
+            networkDataManager.OnMessageReceived(sender, e);
+        }
+        private void InitialiseStreaks(bool readFromSave = false)
+        {
+            List<string> npcNames = new List<string>();
+            foreach (NPC npc in Utility.getAllVillagers())
+            {
+                if (!npcNames.Contains(npc.Name))
+                {
+                    if (!npc.CanSocialize)
+                        continue;
+
+                    npcNames.Add(npc.Name);
+                    if (readFromSave)
+                    {
+                        FriendshipStreak streak = Helper.Data.ReadSaveData<FriendshipStreak>(npc.Name);
+                        if (streak == null)
+                        {
+                            Monitor.Log($"No streak found for {npc.Name}. Initialising new one...");
+                            streak = new FriendshipStreak(npc.Name, 0, 0, 0, 0);
+                        }
+                        streaks.Add(npc.Name, streak);
+                    }
+                    else
+                    {
+                        FriendshipStreak streak = new FriendshipStreak(npc.Name, 0, 0, 0, 0);
+                        streaks.Add(npc.Name, streak);
+                    }
+                }
+            }
+        }
         private void SetStreak(string command, string[] args)
         {
 
@@ -103,26 +165,36 @@ namespace FriendshipStreaks
             }
         }
 
+        private void OnPeerConnected(object sender, PeerConnectedEventArgs e)
+        {
+            networkDataManager.SendDataToFarmhand(e.Peer.PlayerID);
+        }
         private void OnSaveLoaded(object sender, SaveLoadedEventArgs e)
         {
             streaks.Clear();
-            List<string> npcNames = new List<string>();
-            foreach (NPC npc in Utility.getAllVillagers())
+            skipResettingStreaks = true; //Only relevant for farmhands in multiplayer
+            if (networkDataManager == null)
+                networkDataManager = new();
+            if (Context.IsMainPlayer && Context.IsMultiplayer)
             {
-                if (!npcNames.Contains(npc.Name))
-                {
-                    if (!npc.CanSocialize)
-                        continue;
+                NetworkDataManager _manager = Helper.Data.ReadSaveData<NetworkDataManager>("NetworkDataManager");
+                if (_manager != null)
+                    networkDataManager = _manager;
 
-                    npcNames.Add(npc.Name);
-                    FriendshipStreak streak = Helper.Data.ReadSaveData<FriendshipStreak>(npc.Name);
-                    if (streak == null)
-                    {
-                        Monitor.Log($"No streak found for {npc.Name}. Initialising new one...");
-                        streak = new FriendshipStreak(npc.Name, 0, 0, 0, 0);
-                    }
-                    streaks.Add(npc.Name, streak);
-                }
+                InitialiseStreaks(true);
+                return;
+            }
+
+            else if (Context.IsMultiplayer)
+            {
+                networkDataManager.RequestData();
+                if (streaks.Count == 0)
+                    InitialiseStreaks();
+            }
+
+            else
+            {
+                InitialiseStreaks(true);
             }
         }
     }
