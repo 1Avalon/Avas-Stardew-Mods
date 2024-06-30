a76fe676cf5fdf840acc42276115d4992c590944
Avalon <axj30.11.04@gmail.com>
2024-06-30 19:59:21 +0100

    init

---
+++ FriendshipStreaks/ModConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendshipStreaks
+{
+    public class ModConfig
+    {
+        public bool enableBonus = true;
+    }
+}
