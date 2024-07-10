59e7fee4e9d0f773b98c1166c0fcf7a89e7d118f
Avalon <axj30.11.04@gmail.com>
2024-07-10 14:26:10 +0100

    init

---
+++ DynamicNPCPaintings/Framework/Background.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicNPCPaintings.Framework
+{
+    public class Background
+    {
+        public int offsetX;
+
+        public int offsetY;
+
+        public Texture2D backgroundImage;
+
+        public Background(int offsetX, int offsetY, Texture2D backgroundImage)
+        {
+            this.offsetX = offsetX;
+            this.offsetY = offsetY;
+            this.backgroundImage = backgroundImage;
+        }
+
+        public static Background GetDefaultBackground()
+        {
+            return new Background(0, 5, ModEntry.background);
+        }
+    }
+}
---
+++ DynamicNPCPaintings/Framework/Frame.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicNPCPaintings.Framework
+{
+    public class Frame
+    {
+        public int startX;
+
+        public int startY;
+
+        public int endX;
+
+        public int endY;
+
+        public Texture2D frameTexture;
+
+        public Frame(int startX, int startY, int endX, int endY, Texture2D frameTexture)
+        {
+            this.startX = startX;
+            this.startY = startY;
+            this.endX = endX;
+            this.endY = endY;
+            this.frameTexture = frameTexture;
+        }
+
+        public static Frame GetDefaultFrame()
+        {
+            return new Frame(4, 5, 45, 25, ModEntry.frame);
+        }
+    }
+}
