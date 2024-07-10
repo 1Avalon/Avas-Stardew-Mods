7ad9d369f2455f459b9dc807e46b56b093d5da0a
Avalon <axj30.11.04@gmail.com>
2024-07-10 15:12:59 +0100

    init

---
+++ DynamicNPCPaintings/Framework/Picture.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using Microsoft.Xna.Framework;
+
+namespace DynamicNPCPaintings.Framework
+{
+    public class Picture
+    {
+        public Frame frame;
+
+        public Background background;
+
+        public NPC target;
+
+        public int npcFrame;
+
+        public int npcOffsetX;
+
+        public int npcOffsetY;
+
+        public Picture(Frame frame, Background background, NPC target, int npcFrame)
+        {
+            this.frame = frame;
+            this.background = background;
+            this.target = target;
+            this.npcFrame = npcFrame;
+            this.npcOffsetX = background.backgroundImage.Width / 2;
+            this.npcOffsetY = 0;
+        }
+
+        public static Picture GetDefaultPicture()
+        {
+            return new Picture(Frame.GetDefaultFrame(), Background.GetDefaultBackground(), Game1.getCharacterFromName("Haley"), 0);
+        }
+        public Texture2D GetTexture()
+        {
+            Texture2D frameAndBackground = TextureHelper.BackgroundWithFrame(frame, background);
+            Texture2D characterTexture = TextureHelper.GetCharacterFrame(target.Sprite.Texture, npcFrame);
+            return TextureHelper.DrawCharacterOnBackground(frameAndBackground, characterTexture, new Vector2(npcOffsetX, npcOffsetY), frame.startX, frame.startY, frame.endX, frame.endY);
+        }
+    }
+}
