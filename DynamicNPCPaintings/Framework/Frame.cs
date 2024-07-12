013228b68b595a5151779363098f09364b538a13
Avalon <axj30.11.04@gmail.com>
2024-07-12 12:25:14 +0100

    adjust for cp compatibility

--- DynamicNPCPaintings/Framework/Frame.cs
+++ DynamicNPCPaintings/Framework/Frame.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DynamicNPCPaintings.Framework
@@ -17,15 +19,44 @@ namespace DynamicNPCPaintings.Framework
 
         public int endY;
 
-        public Texture2D frameTexture;
+        public int spaceWidth;
 
-        public Frame(int startX, int startY, int endX, int endY, Texture2D frameTexture)
+        public int spaceHeight;
+
+        public string textureName;
+
+        private Texture2D _frameTexture;
+
+        public Texture2D frameTexture
+        {
+            get
+            {
+                return _frameTexture ?? ModEntry.instance.Helper.GameContent.Load<Texture2D>(textureName);
+            }
+        }
+        public static Frame GetFrameWithTexture(int startX, int startY, int endX, int endY, Texture2D frameTexture)
+        {
+            return new Frame(startX, startY, endX, endY, frameTexture);
+        }
+        private Frame(int startX, int startY, int endX, int endY, Texture2D frameTexture)
         {
             this.startX = startX;
             this.startY = startY;
             this.endX = endX;
             this.endY = endY;
-            this.frameTexture = frameTexture;
+            _frameTexture = frameTexture;
+            spaceWidth = endX - startX; 
+            spaceHeight = endY - startY;
+        }
+        public Frame(int startX, int startY, int endX, int endY, string textureName)
+        {
+            this.startX = startX;
+            this.startY = startY;
+            this.endX = endX;
+            this.endY = endY;
+            this.textureName = textureName;
+            spaceWidth = endX - startX;
+            spaceHeight = endY - startY;
         }
 
         public static Frame GetDefaultFrame()
