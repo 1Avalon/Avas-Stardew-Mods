42db15fd1416a801b0b4e67ff8262653dfbedaa2
Avalon <axj30.11.04@gmail.com>
2024-07-12 19:04:34 +0100

    add namespace

--- DynamicNPCPaintings/UI/SelectNPCMenu.cs
+++ DynamicNPCPaintings/UI/SelectNPCMenu.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using DynamicNPCPaintings.UI.UIElements;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewValley;
 using StardewValley.Menus;
