10690ddb4450e80546e2efd362490a4adbe066e6
Avalon <axj30.11.04@gmail.com>
2024-07-14 17:53:16 +0100

    add property

--- DynamicNPCPaintings/UI/UIElements/Button.cs
+++ DynamicNPCPaintings/UI/UIElements/Button.cs
@@ -21,14 +21,17 @@ namespace DynamicNPCPaintings.UI.UIElements
 
         public int height;
 
+        public bool active;
+
         public Color textColor = Game1.textColor;
-        public Button(string label, Action action) : base(Rectangle.Empty, null, Rectangle.Empty, 1f)
+        public Button(string label, Action action, bool isActive = true) : base(Rectangle.Empty, null, Rectangle.Empty, 1f)
         {
             Label = label;
             Action = action;
             width = (int)Game1.dialogueFont.MeasureString(label).X + 64;
             height = 68;
             bounds = new Rectangle(0, 0, width, height);
+            active = isActive;
         }
         public void SetPosition(int x, int y)
         {
@@ -36,10 +39,14 @@ namespace DynamicNPCPaintings.UI.UIElements
         }
         public void CallEvent()
         {
-            Action();
+            if (active)
+                Action();
         }
         public override void draw(SpriteBatch b)
         {
+            if (!active)
+                return;
+
             float draw_layer = 0.8f - (bounds.X + bounds.Y) * 1E-06f;
             IClickableMenu.drawTextureBox(b, Game1.mouseCursors, new Rectangle(432, 439, 9, 9), bounds.X, bounds.Y, bounds.Width, bounds.Height, Color.White, 4f, drawShadow: true, draw_layer);
             Vector2 string_center = Game1.dialogueFont.MeasureString(Label) / 2f;
