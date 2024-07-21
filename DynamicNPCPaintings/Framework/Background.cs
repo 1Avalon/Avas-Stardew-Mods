9bde2f0a28f6cf98879b8c493e7d0adf1220accf
Avalon <axj30.11.04@gmail.com>
2024-07-21 11:33:52 +0100

    add content patcher compatibility

--- DynamicNPCPaintings/Framework/Background.cs
+++ DynamicNPCPaintings/Framework/Background.cs
@@ -13,18 +13,36 @@ namespace DynamicNPCPaintings.Framework
 
         public int offsetY;
 
-        public Texture2D backgroundImage;
+        public string textureName;
 
-        public Background(int offsetX, int offsetY, Texture2D backgroundImage)
+        public Texture2D backgroundImage { get => _backgroundImage ?? ModEntry.instance.Helper.GameContent.Load<Texture2D>(textureName); }
+
+        private Texture2D _backgroundImage;
+
+        private Background(int offsetX, int offsetY, Texture2D backgroundImage)
         {
             this.offsetX = offsetX;
             this.offsetY = offsetY;
-            this.backgroundImage = backgroundImage;
+            this._backgroundImage = backgroundImage;
         }
 
+        public static Background Of(int offsetX, int offsetY, Texture2D backgroundImage)
+        {
+            return new Background(offsetX, offsetY, backgroundImage);
+        }
+
+        public Background(string textureName)
+        {
+            this.textureName = textureName;
+        }
+
+        public bool FitsInFrame(Frame frame)
+        {
+            return frame.spaceWidth <= this.backgroundImage.Width && frame.spaceHeight <= this.backgroundImage.Height;
+        }
         public static Background GetDefaultBackground()
         {
-            return new Background(0, 5, ModEntry.backgroundImages["forest"]);
+            return new Background(0, 5, ModEntry.backgroundImages["Forest"]);
         }
     }
 }
