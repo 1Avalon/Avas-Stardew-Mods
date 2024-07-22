d4354f8fec926aa5397f347fb12e9f0bd0b09147
Avalon <axj30.11.04@gmail.com>
2024-07-22 10:54:16 +0100

    add displayName property

--- CustomNPCPaintings/Framework/Background.cs
+++ CustomNPCPaintings/Framework/Background.cs
@@ -19,21 +19,24 @@ namespace DynamicNPCPaintings.Framework
 
         private Texture2D _backgroundImage;
 
-        private Background(int offsetX, int offsetY, Texture2D backgroundImage)
+        public string displayName;
+        private Background(string displayName, int offsetX, int offsetY, Texture2D backgroundImage)
         {
             this.offsetX = offsetX;
             this.offsetY = offsetY;
             this._backgroundImage = backgroundImage;
+            this.displayName = displayName;
         }
 
-        public static Background Of(int offsetX, int offsetY, Texture2D backgroundImage)
+        public static Background Of(string displayName, int offsetX, int offsetY, Texture2D backgroundImage)
         {
-            return new Background(offsetX, offsetY, backgroundImage);
+            return new Background(displayName, offsetX, offsetY, backgroundImage);
         }
 
-        public Background(string textureName)
+        public Background(string textureName, string displayName)
         {
             this.textureName = textureName;
+            this.displayName = displayName;
         }
 
         public bool FitsInFrame(Frame frame)
@@ -42,7 +45,7 @@ namespace DynamicNPCPaintings.Framework
         }
         public static Background GetDefaultBackground()
         {
-            return new Background(0, 5, ModEntry.backgroundImages["Forest"]);
+            return new Background("Forest", 0, 5, ModEntry.backgroundImages["DefaultImg.Forest"].backgroundImage);
         }
     }
 }
