419f8dcea21fafdc0ec81be375463a6d1dd73da4
Avalon <axj30.11.04@gmail.com>
2024-07-25 17:09:37 +0100

    init

---
+++ CustomNPCPaintings/ModConfig.cs
@@ -0,0 +1,14 @@
+﻿using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomNPCPaintings
+{
+    public class ModConfig
+    {
+        public SButton openCustomizerButton { get; set; }
+    }
+}
