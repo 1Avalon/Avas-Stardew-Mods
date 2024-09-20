3a3259e293913117e675846b9609394c36725294
Avalon <axj30.11.04@gmail.com>
2024-09-20 12:31:36 +0100

    add commands

--- ImprovedFallDebris/ModEntry.cs
+++ ImprovedFallDebris/ModEntry.cs
@@ -7,6 +7,7 @@ using HarmonyLib;
 using StardewValley;
 using Microsoft.Xna.Framework.Graphics;
 using System.Diagnostics.Tracing;
+using System.Runtime.InteropServices;
 
 namespace ImprovedFallDebris
 {
@@ -28,6 +29,9 @@ namespace ImprovedFallDebris
                 original: AccessTools.Method(typeof(WeatherDebris), nameof(WeatherDebris.draw)),
                 prefix: new HarmonyMethod(typeof(Patches), nameof(Patches.Prefix_draw))
                 );
+
+            Helper.ConsoleCommands.Add("clear_debris_array", "Clears the colored fall leaves debris. Not an interesting command for a player", this.ClearDebrisArray);
+            Helper.ConsoleCommands.Add("add_debris_color", "Adds a new color choice to the array. Not an interesting command for a player", this.AddColorToDebrisArray);
         }
         /*********
         ** Private methods
@@ -35,5 +39,33 @@ namespace ImprovedFallDebris
         /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event data.</param>
+        /// 
+        private void ClearDebrisArray(string command, string[] args)
+        {
+            Patches.customDebrisTextures.Clear();
+            Monitor.Log("Successfully cleared the custom debris", LogLevel.Info);
+        }
+        private void AddColorToDebrisArray(string command, string[] args)
+        {
+            float hueShift = float.Parse(args[0]);
+            int saturation = int.Parse(args[1]);
+            int brightness = int.Parse(args[2]);
+
+            Vector3 values = new Vector3(hueShift, saturation, brightness);
+
+            int index;
+            if (args.Length >= 4)
+            {
+                index = int.Parse(args[3]);
+                Patches.Adjustments[index] = values;
+                Monitor.Log($"Successfully replaced the values at index {index}.", LogLevel.Info);
+            }
+            else
+            {
+                Patches.Adjustments.Add(values);
+                Monitor.Log("Successfully added a new color. Please clear the array using the corresponding command", LogLevel.Info);
+            }
+            Patches.customDebrisTextures.Clear();
+        }
     }
 }
