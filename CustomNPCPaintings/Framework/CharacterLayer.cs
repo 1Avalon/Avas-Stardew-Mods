d7f5ff6bf4f3fe60d6b5add87eee8c76c13d6155
Avalon <axj30.11.04@gmail.com>
2024-10-30 22:40:28 +0100

    init

---
+++ CustomNPCPaintings/Framework/CharacterLayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynamicNPCPaintings.Framework;
+using StardewValley;
+
+namespace CustomNPCPaintings.Framework
+{
+    public class CharacterLayer
+    {
+        public int layer;
+
+        public NPC target;
+        public int npcFrameAmount { get => (target.Sprite.Texture.Width / target.Sprite.SpriteWidth) * (target.Sprite.Texture.Height / target.Sprite.SpriteHeight); }
+
+        public int npcFrame;
+
+        public int npcOffsetX;
+
+        public int npcOffsetY;
+
+        public bool npcFlipped = false;
+
+        public CharacterLayer(NPC target, DynamicNPCPaintings.Framework.Background background, int npcFrame) 
+        { 
+            this.target = target;
+            this.npcFrame = npcFrame;
+            this.npcOffsetX = background.backgroundImage.Width / 2; ;
+            this.npcOffsetY = 0;
+        }
+    }
+}
