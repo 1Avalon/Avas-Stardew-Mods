e47b2a47f865f68dcfed6d297ba0ab10a3abb7f4
Avalon <axj30.11.04@gmail.com>
2025-01-19 11:16:43 +0100

    remove event handler from template

--- MoreSaveInformation/ModEntry.cs
+++ MoreSaveInformation/ModEntry.cs
@@ -20,7 +20,6 @@ namespace MoreSaveInformation
         public static Mod instance;
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
             instance = this;
 
             Harmony harmony = new(this.ModManifest.UniqueID);
@@ -29,23 +28,6 @@ namespace MoreSaveInformation
                 original: AccessTools.Method(typeof(SaveFileSlot), nameof(SaveFileSlot.Draw)),
                 postfix: new HarmonyMethod(typeof(Patches), nameof(Patches.Postfix_DrawSaveFileSlot))
                 );
-        }
-
-
-        /*********
-        ** Private methods
-        *********/
-        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
-        {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
-
-            // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
         }
     }
 }
