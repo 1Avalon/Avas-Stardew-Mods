2666bcfa86707b659884db9185f7a2d0560f7da4
Avalon <axj30.11.04@gmail.com>
2025-03-15 13:28:42 +0100

    init

---
+++ PrismaticQuality/ModEntry.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using HarmonyLib;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PrismaticQuality
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        /// 
+
+        public static Texture2D PrismaticStarTexture;
+
+        public static Mod instance;
+
+        public override void Entry(IModHelper helper)
+        {
+            instance = this;
+
+            helper.Events.Display.Rendered += this.OnRendered;
+
+            PrismaticStarTexture = Helper.ModContent.Load<Texture2D>("assets/PrismaticStarTexture.png");
+
+            Harmony harmony = new Harmony(this.ModManifest.UniqueID);
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(GameLocation), nameof(GameLocation.GetHarvestSpawnedObjectQuality)),
+                postfix: new HarmonyMethod(typeof(Patches), nameof(Patches.Postfix_GetHarvestObjectQuality))
+            );
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(Item), nameof(Item.DrawMenuIcons)),
+                postfix: new HarmonyMethod(typeof(Patches), nameof(Patches.DrawIconsMenu_Postfix))
+            );
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(Game1), nameof(Game1.createItemDebris)),
+                prefix: new HarmonyMethod(typeof(Patches), nameof(Patches.Prefix_createItemDebris))
+            );
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(Farmer), nameof(Farmer.addItemToInventoryBool)),
+                prefix: new HarmonyMethod(typeof(Patches), nameof(Patches.Prefix_addItemToInventory))
+            );
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnRendered(object sender, RenderedEventArgs e)
+        {
+            e.SpriteBatch.Draw(PrismaticStarTexture, new Rectangle(0, 0, 8, 8), Color.White);
+        }
+    }
+}
---
+++ PrismaticQuality/Patches.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismaticQuality
+{
+    public static class Patches
+    {
+        public static void Postfix_GetHarvestObjectQuality(GameLocation __instance, ref int __result, Farmer who, bool isForage, Vector2 tile, Random random = null)
+        {
+            if (who.professions.Contains(16) && isForage)
+            {
+                __result = 5;
+            }
+        }
+
+        public static void Prefix_createItemDebris(ref Item item, Vector2 pixelOrigin, int direction, GameLocation location = null, int groundLevel = -1, bool flopFish = false)
+        {
+            if (item.Quality == 0)
+                item.Quality = 5;
+        }
+
+        public static void Prefix_addItemToInventory(ref Item item, bool makeActiveObject = false)
+        {
+            if (item.Quality == 0)
+                item.Quality = 5;
+        }
+
+        public static void DrawIconsMenu_Postfix(Item __instance, SpriteBatch sb, Vector2 location, float scale_size, float transparency, float layer_depth, StackDrawType drawStackNumber, Color color)
+        {
+            if (__instance.quality.Value == 5)
+            {
+                var qualityRect = new Rectangle(0, 0, 8, 8);
+                var qualitySheet = ModEntry.PrismaticStarTexture;
+                sb.Draw(qualitySheet, location + new Vector2(12f, 52f), qualityRect, color * transparency, 0f, new Vector2(4f, 4f), 3f * scale_size, SpriteEffects.None, layer_depth);
+            }
+        }
+    }
+}
