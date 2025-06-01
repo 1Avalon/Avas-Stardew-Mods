32b33ef1b998de84999a7ba345d379baa371e73d
Avalon <axj30.11.04@gmail.com>
2025-06-01 09:39:58 +0100

    fix scaling for any ui scale and zoom settings

--- CustomNPCPaintings/UI/Customiser.cs
+++ CustomNPCPaintings/UI/Customiser.cs
@@ -46,7 +46,10 @@ namespace DynamicNPCPaintings.UI
         {
             int width = 960;
             int height = 720;
-            base.initialize(Game1.viewport.Width / 2 - width / 2, Game1.viewport.Height / 2 - height / 2, width, height);
+
+            Vector2 center = Utility.getTopLeftPositionForCenteringOnScreen(width, height);
+
+            base.initialize((int)center.X, (int)center.Y, width, height);
 
             previewTexture = picture.GetTexture();
             preview = new ClickableTextureComponent(new Rectangle(xPositionOnScreen + 50, yPositionOnScreen + 120, 48, 32), previewTexture, new Rectangle(0, 0, 48, 32), 6f);
