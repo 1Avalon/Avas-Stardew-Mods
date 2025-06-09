48b5aa0584fc97b6b52c4ae117bc37f0195d45a9
Avalon <axj30.11.04@gmail.com>
2025-06-09 10:29:21 +0100

    init

---
+++ FriendshipBars/ModEntry.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using HarmonyLib;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley.Menus;
+
+namespace FriendshipBars
+{
+    /// <summary>The mod entry point.</summary>
+    internal sealed class ModEntry : Mod
+    {
+        /*********
+        ** Public methods
+        *********/
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        /// 
+
+        public static Texture2D progressBar;
+
+        public override void Entry(IModHelper helper)
+        {
+            Harmony harmony = new Harmony(this.ModManifest.UniqueID);
+
+            harmony.Patch(
+                original: AccessTools.Method(typeof(SocialPage), nameof(SocialPage.drawNPCSlot)),
+                postfix: new HarmonyMethod(typeof(Patches), nameof(Patches.Postfix_drawNPCSlot))
+                );
+
+            progressBar = Helper.ModContent.Load<Texture2D>("assets\\progress_bar.png");
+        }
+
+
+        /*********
+        ** Private methods
+        *********/
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            // print button presses to the console window
+            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+        }
+    }
+}
---
+++ FriendshipBars/Patches.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using StardewValley.Menus;
+using static StardewValley.Menus.SocialPage;
+
+namespace FriendshipBars
+{
+    public static class Patches
+    {
+        private static Texture2D rectText;
+        public static void Postfix_drawNPCSlot(SocialPage __instance, SpriteBatch b, int i)
+        {
+            SocialEntry entry = __instance.GetSocialEntry(i);
+
+            if (entry.Friendship == null)
+                return;
+
+            float currentPoints = entry.Friendship.Points % 250;
+            float requiredPoints = 250;
+
+            if (rectText ==  null)
+            {
+                rectText = new Texture2D(Game1.graphics.GraphicsDevice, 1, 1);
+                rectText.SetData(new Color[] { new Color(216, 55, 0) });
+            }
+            b.Draw(ModEntry.progressBar, new Vector2(__instance.xPositionOnScreen + 320 - 4 + 32 * 5 - 46, __instance.sprites[i].bounds.Y), null, Color.White, 0f, Vector2.Zero, 2f, SpriteEffects.None, 5f);
+
+            float completion = currentPoints / requiredPoints;
+
+            int progressBarWidth = (int)Math.Floor(completion * 42 * 2);
+
+            b.Draw(rectText, new Rectangle(__instance.xPositionOnScreen + 320 - 4 + 32 * 5 - 46 + 4, __instance.sprites[i].bounds.Y + 4, progressBarWidth, 16), Color.White);
+        }
+    }
+}
