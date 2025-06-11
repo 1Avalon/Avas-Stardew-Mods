147d2060d84f81fe9efb272387c5ed4aed74a889
Avalon <axj30.11.04@gmail.com>
2025-06-11 11:19:44 +0100

    add Config

---
+++ FriendshipBars/ModConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendshipBars
+{
+    public class ModConfig
+    {
+        public bool Enabled { get; set; } = true;
+
+        public bool CurrentPointsHover { get; set; } = true;
+
+        public bool RequiredPointsHover { get; set; } = true;
+
+        public bool CompletionHover { get; set; } = true;
+    }
+}
--- FriendshipBars/ModEntry.cs
+++ FriendshipBars/ModEntry.cs
@@ -7,6 +7,7 @@ using StardewValley;
 using HarmonyLib;
 using Microsoft.Xna.Framework.Graphics;
 using StardewValley.Menus;
+using GenericModConfigMenu;
 
 namespace FriendshipBars
 {
@@ -22,9 +23,13 @@ namespace FriendshipBars
 
         public static Texture2D progressBar;
 
+        public static ModConfig Config;
+
         public override void Entry(IModHelper helper)
         {
             Harmony harmony = new Harmony(this.ModManifest.UniqueID);
+
+            Config = Helper.ReadConfig<ModConfig>();
 
             harmony.Patch(
                 original: AccessTools.Method(typeof(SocialPage), nameof(SocialPage.drawNPCSlot)),
@@ -41,8 +46,63 @@ namespace FriendshipBars
                 postfix: new HarmonyMethod(typeof(Patches), nameof(Patches.Postfix_performHoverAction))
                 );
 
+            Helper.Events.GameLoop.GameLaunched += OnGameLaunched;
+
 
             progressBar = Helper.ModContent.Load<Texture2D>("assets\\progress_bar.png");
+        }
+
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            // get Generic Mod Config Menu's API (if it's installed)
+            var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+                return;
+
+            // register mod
+            configMenu.Register(
+                mod: this.ModManifest,
+                reset: () => Config = new ModConfig(),
+                save: () => this.Helper.WriteConfig(Config)
+            );
+
+            // add some config options
+            configMenu.AddBoolOption(
+                mod: this.ModManifest,
+                name: () => "Enabled",
+                tooltip: () => "Enable or disable the bars",
+                getValue: () => Config.Enabled,
+                setValue: value => Config.Enabled = value
+            );
+
+            configMenu.AddSectionTitle(
+                mod: this.ModManifest,
+                text: () => "Hover Settings"
+            );
+
+            configMenu.AddBoolOption(
+                mod: this.ModManifest,
+                name: () => "Show Current Points",
+                tooltip: () => "Shows your current Points",
+                getValue: () => Config.CurrentPointsHover,
+                setValue: value => Config.CurrentPointsHover = value
+            );
+
+            configMenu.AddBoolOption(
+                mod: this.ModManifest,
+                name: () => "Show Required Points",
+                tooltip: () => "Shows Points required for the next Heart",
+                getValue: () => Config.RequiredPointsHover,
+                setValue: value => Config.RequiredPointsHover = value
+            );
+
+            configMenu.AddBoolOption(
+                mod: this.ModManifest,
+                name: () => "Show Completion",
+                tooltip: () => "Shows the Progress for the next heart in %",
+                getValue: () => Config.CompletionHover,
+                setValue: value => Config.CompletionHover = value
+            );
         }
     }
 }
