637a7bec8d5cff8e2a8b3a24e1539923aed25d34
Avalon <axj30.11.04@gmail.com>
2025-06-11 11:05:15 +0100

    remove event handler from template

--- FriendshipBars/ModEntry.cs
+++ FriendshipBars/ModEntry.cs
@@ -44,22 +44,5 @@ namespace FriendshipBars
 
             progressBar = Helper.ModContent.Load<Texture2D>("assets\\progress_bar.png");
         }
-
-
-        /*********
-        ** Private methods
-        *********/
-        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
-        {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
-
-            // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
-        }
     }
 }
